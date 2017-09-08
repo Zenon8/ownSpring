0ef9a48014620316f228ae21f5be87cd5c928800
Andrii Bezruchko <Andrii_Bezruchko@epam.com>
2017-09-08 16:48:57 +0300

    Add generateBeanDefinition() to BeanDescriptionConfigReader()

--- src/main/java/ua/rd/ioc/BeanDescriptionConfigReader.java
+++ src/main/java/ua/rd/ioc/BeanDescriptionConfigReader.java
@@ -1,15 +1,27 @@
-package ua.allugard.ioc;
+package ua.rd.ioc;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class BeanDescriptionConfigReader implements Config {
+public class BeanDescriptionConfigReader implements ConfigReader {
+
+    private List<BeanDefinition> beanDefinitions = null;
 
     @Override
-    public BeanDefinition[] beanDefinitions() {
-        return new BeanDefinition[0];
+    public List<BeanDefinition> beanDefinitions() {
+
+        if (beanDefinitions == null) {
+            throw new NoBeanDefinitionException();
+        }
+        return beanDefinitions;
     }
 
-    private void createBeanDefinitionByBeanDescription(List<BeanDescription> list) {
+
+    private void generateBeanDefinitionsByBeanDescriptions(List<BeanDescription> list) {
+
+        beanDefinitions = new ArrayList<>();
+        beanDefinitions.addAll(list.stream().map(this::generateBeanDefinition).collect(Collectors.toList()));
 
 //        Class<?> cls = null;
 //
@@ -28,5 +40,18 @@ public class BeanDescriptionConfigReader implements Config {
 //
 //
 //        Object o = c.newInstance();
+    }
+
+    private BeanDefinitionImpl generateBeanDefinition(BeanDescription beanDescription) {
+        BeanDefinitionImpl beanDefinition = new BeanDefinitionImpl();
+        beanDefinition.setId(beanDescription.getId());
+        beanDefinition.setName(beanDescription.getName());
+        beanDefinition.setClassName(beanDescription.getClassName());
+        beanDefinition.setConstrArg(beanDescription.getConstrArg());
+        beanDefinition.setProperties(beanDescription.getProperties());
+        beanDefinition.setInitMethod(beanDescription.getInitMethod());
+        beanDefinition.setDestroyMethod(beanDescription.getDestroyMethod());
+        return beanDefinition;
+
     }
 }
