1e96be04f5492445b1f4daeb80b191b904d85a0c
Andrii Bezruchko <Andrii_Bezruchko@epam.com>
2017-09-08 13:45:11 +0300

    Quick fix in Context and ApplicationContext

--- src/main/java/ua/rd/ioc/ApplicationContext.java
+++ src/main/java/ua/rd/ioc/ApplicationContext.java
@@ -12,32 +12,29 @@ public class ApplicationContext implements Context {
     }
 
     public ApplicationContext() {
-        beanDefinitions =Config.EMPTY_BEANDEFINITIONS;//new BeanDefinition[0];
+        beanDefinitions = Config.EMPTY_BEANDEFINITIONS;//new BeanDefinition[0];
 
     }
 
-    public Object getBean(String beanName) {
+    public Object getBean(String beanName) throws IllegalAccessException, InstantiationException {
         List<BeanDefinition> beanDefinitions =
                 Arrays.asList(this.beanDefinitions);
-        if(beanDefinitions.stream().map(BeanDefinition::getBeanName).anyMatch(n -> n.equals(beanName))){
-           // return new Object();
-            BeanDefinition beanDefinition=null;
-            try {
-                return beanDefinition.getBeanType().newInstance();
+        if (beanDefinitions.stream().map(BeanDefinition::getBeanName).anyMatch(n -> n.equals(beanName))) {
+            // return new Object();
+            BeanDefinition beanDefinition = null;
+            return beanDefinition.getBeanType().newInstance();
 
-            } catch (InstantiationException|IllegalAccessException  e) {
-                e.printStackTrace();
-            }
-        }
-        else {
+        } else {
+
             throw new NoSuchBeanException();
         }
+
     }
 
     public String[] getBeanDefinitionNames() {
-        String [] beanDefinitionNames= new String[beanDefinitions.length];
-        for (int i = 0; i <beanDefinitions.length ; i++) {
-            beanDefinitionNames[i]= beanDefinitions[i].getBeanName();
+        String[] beanDefinitionNames = new String[beanDefinitions.length];
+        for (int i = 0; i < beanDefinitions.length; i++) {
+            beanDefinitionNames[i] = beanDefinitions[i].getBeanName();
         }
         return beanDefinitionNames;
     }
