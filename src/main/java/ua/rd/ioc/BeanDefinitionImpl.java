ffbf792e6abfd4ec394b1253d1a5f2259360d4fb
Andrii Bezruchko <Andrii_Bezruchko@epam.com>
2017-09-08 16:49:40 +0300

    Add fields to Bean Definition

--- src/main/java/ua/rd/ioc/BeanDefinitionImpl.java
+++ src/main/java/ua/rd/ioc/BeanDefinitionImpl.java
@@ -1,4 +1,32 @@
 package ua.rd.ioc;
 
-public class BeanDefinitionImpl {
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Getter
+@Setter
+public class BeanDefinitionImpl implements BeanDefinition {
+
+    private String id;
+    private String name;
+    private String className;
+    private String initMethod;
+    private String destroyMethod;
+    private LinkedHashMap<Class<?>, Object> constrArg = new LinkedHashMap<>();
+    private Map<String, Object> properties = new HashMap<>();
+
+
+    @Override
+    public String getBeanName() {
+        return null;
+    }
+
+    @Override
+    public Class<?> getBeanType() {
+        return null;
+    }
 }
